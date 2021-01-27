5fcbe87ad93227ab0747ca3baa61b5074cfd8cf5
Bleak Chandler <bleakchandler@gmail.com>
2021-01-27 21:36:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,53 @@
 # require modules here
 
-def load_library
-  # code goes here
+
+require "pry"
+require "yaml"
+
+
+def load_library(path)
+ path = YAML.load_file("lib/emoticons.yml")
+  updated_emoticon_library = {}
+     path.each do |emoji_name, emoji|
+      updated_emoticon_library[emoji_name] = {:english => {}, :japanese => {}}
+       emoji.each do |emojinew|
+         if updated_emoticon_library[emoji_name][:english] == {}
+            updated_emoticon_library[emoji_name][:english] = emojinew
+        else 
+            updated_emoticon_library[emoji_name][:japanese] = emojinew
+       end
+     end
+   end
+  # p "#{updated_emoticon_library}"
+  updated_emoticon_library
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(path, emoticon)
+   get_emoticons = load_library(path)
+   p "hi"
+   if h = get_emoticons.find { |h| h['product'] == 'bcx' }
+     p "worked"
+  else
+    puts 'Not found!'
+  end
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(path, emoticon)
+   get_emoticons = load_library(path)
+  if get_emoticons.has_key?(emoticon)
+     get_emoticons[emoji_name][emoticon]
+   else
+     return "Sorry, that emoticon was not found"
+  end
 end
+
+
+
+
+#hash should look like the below
+#{
+ #     'happy' => {
+  #       :english => ":)",
+   #      :japanese => "(＾ｖ＾)"
+#      }
+#   }
