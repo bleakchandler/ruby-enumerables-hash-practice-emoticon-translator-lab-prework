bc5f3e64e8a35c1a988cbcff25cda4088dbb0584
Bleak Chandler <bleakchandler@gmail.com>
2021-01-27 22:15:06 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -53,7 +53,7 @@ def get_japanese_emoticon(path, emoticon)
   get_emoticons.each do |key, value|
     if emoticon == value
       return answer = key
-      p key
+      p answer
     else
        answer = "Sorry, that emoticon was not found"
     end
