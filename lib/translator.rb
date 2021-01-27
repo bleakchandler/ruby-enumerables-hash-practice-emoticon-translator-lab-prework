0ba0ab10c764177045cf22fb1590913c8535b747
Bleak Chandler <bleakchandler@gmail.com>
2021-01-27 22:08:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -69,7 +69,7 @@ def get_english_meaning(path, emoticon)
       return key
     #  p key
     else
-      key = "Sorry, that emoticon was not found"
+      return key = "Sorry, that emoticon was not found"
     end
   end
     key
