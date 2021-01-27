0b1f8572d1b2bfc3c4817c24e28caf64b1133d8a
Bleak Chandler <bleakchandler@gmail.com>
2021-01-27 21:52:20 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,7 +24,7 @@ end
 
 def get_japanese_emoticon(path, emoticon)
    get_emoticons = load_library(path)
-   japanese_emoticon = get_emoticons["angel"]
+   japanese_emoticon = get_emoticons["happy"]
    p japanese_emoticon
    p values
    # p "values"
